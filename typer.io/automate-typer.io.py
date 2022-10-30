b375850ae8997115dd5e2b149fe056fee1e7bde3
Ashish Singh <ashish1qwerty1@gmail.com>
2022-10-30 14:20:30 +0530

    Final update

--- typer.io/automate-typer.io.py
+++ typer.io/automate-typer.io.py
@@ -2,15 +2,15 @@ import time
 
 from selenium import webdriver
 
-# this imports are for explicit wait
+# below imports are for explicit wait
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait # -> this is wrong import
 from selenium.webdriver.support.wait import WebDriverWait # -> this is the correct import
 from selenium.webdriver.support import expected_conditions
 
-
 import os
 
+# keep the driver file in the project folder itself
 os.environ['PATH'] += "msedgedriver.exe"
 
 options = webdriver.EdgeOptions()
@@ -20,36 +20,31 @@ driver = webdriver.Edge(options = options)
 
 driver.get("https://typer.io")
 
-# this is for maximizing the window
+# maximize the window
 driver.maximize_window()
 
+# implicitly wait for the page to completely load
 driver.implicitly_wait(30)
 
 quickPlay = driver.find_element("xpath", '//*[@id="__next"]/main/div/section[1]/div[2]/a[1]')
 quickPlay.click()
 
-# #scroll by pixel
-# driver.execute_script("window.scrollBy(0,200)","")
-
 # time.sleep(3)
 # here also we don't want to wait for 3 seconds to scroll when the play site has been opened
 # so apply explicit wait here
 WebDriverWait(driver, 5).until(expected_conditions.url_matches("https://typer.io/play"))
-# here use url_matches, not url_changes
-# url_changes is used to check if the previous ulr changed
-# url_matches checks if the current url is matched or not
+# here use url_matches(), not url_changes()
+# url_changes() is used to check if the previous ulr changed
+# url_matches() checks if the current url is matched or not
 
-try:
-    driver.execute_script("scrollBy(0,-1000);") # -> this is for scrolling, see -ve sign for
-    print("scrolling successful!") #  scrolling up, use +ve for scrolling down, it is the value of pixels
-except:
-    print("scrolling failed!")
+# Scrolling up
+driver.execute_script("scrollBy(0,-1000);")
+
+# wait for the page to completely load before taking the source
 time.sleep(3)
-
 source = driver.page_source
 
-
-
+# Scrape the text
 from bs4 import BeautifulSoup
 import lxml
 
@@ -58,38 +53,11 @@ soup = BeautifulSoup(source, 'lxml')
 text_ = soup.find('div', class_ = "Gameboard_container__QyjKg")
 wordList = [i.text for i in text_]
 
-print(len(wordList))
-print(wordList)
-
-
-# time.sleep(15) we don't want to wait by time, we wait till go appears
-# we want to wait till the timer starts, and then start typing.
-
-
-'''
-# initiate explicit wait
-WebDriverWait(driver, 15).until(
-    expected_conditions.text_to_be_present_in_element(
-        # first argument is text to check, then second is text to appear
-        (By.XPATH, '//*[@id="__next"]/div[2]/div[1]/div[2]/div[2]/h3'),
-        'GO!' # -> this is the text we wait till it appears
-    )
-)
-# when "GO! will appear we imeaditly execute the following task
-'''
-
-# initiate explicit wait
+# initiate explicit wait, (wait till GO! has appeared)
 WebDriverWait(driver, 20).until(expected_conditions.text_to_be_present_in_element((By.XPATH, '//*[@id="__next"]/div[2]/div[1]/div[2]/div'), "GO!"))
 
-try:
-    type_here_when_the_game_begins  = driver.find_element("xpath", '//*[@id="__next"]/div[2]/div[1]/div[4]/div/div')
-    type_here_when_the_game_begins.click()
-    print("clikc succefull!")
-except:
-    print("element not found!")
-
-
-# time.sleep(17)
+type_here_when_the_game_begins  = driver.find_element("xpath", '//*[@id="__next"]/div[2]/div[1]/div[4]/div/div')
+type_here_when_the_game_begins.click()
 
 import pyautogui
 
@@ -98,5 +66,6 @@ for j in wordList:
     pyautogui.write(j)
     pyautogui.press("space")
 
+# close the browser
 time.sleep(20)
 driver.close()
